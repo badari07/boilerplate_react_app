047adeec464abe98a80c79c0ef13851934b0ba59
badari <badari0793@gmail.com>
2019-04-30 12:59:59 +0530

    boilerplate

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Router ,Route, Switch,Link,NavLink} from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import NotFound from '../components/NotFoundPage';
+import DashboardPage from '../components/DashboardPage';
+import LoginPage from '../components/LoginPage';
+import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
+
+
+export const history = createBrowserHistory();
+const AppRouter=()=> (
+    <Router history={history}>
+        <div>
+            <Switch>
+                <PublicRoute path="/" component={LoginPage} exact={true}/>
+                <PrivateRoute path="/dashboard" component={DashboardPage}/>
+                <Route component={NotFound}/>
+            </Switch>
+        </div>
+    </Router>
+);
+export default AppRouter;
